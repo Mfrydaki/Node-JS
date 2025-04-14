a380de9d985f9742a28c75ffc1b0841c5c06e27d
Maria Frydaki <mfrydaki@gmail.com>
2025-04-14 17:09:05 +0300

    updates

---
+++ routes/auth.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const authController = require('../controllers/auth.controller');
+
+router.post('/login', authController.login);
+router.get('/google/callback', authController.googleLogin);
+
+module.exports = router
+
